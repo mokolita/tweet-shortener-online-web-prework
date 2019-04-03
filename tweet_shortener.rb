02287cda8af3838cd06915eb75afb5d925fd1b88
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:31:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,27 +1,34 @@
-
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
 
 def word_substituter(tweet)
-  dict = dictionary
-  words = tweet.split(" ")
 
-  words.collect do |word|
-    dictionary.collect do |key, value|
-      if word == key 
-        word.replace(value)
-      end 
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
     end
-  end 
-  words.join(" ")
-end 
+  end.join(" ")
+end
 
-
-def bulk_tweet_shortener(tweets_array)
-
-  tweets_array.collect do |tweet|
-    puts word_substituter(tweet)
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
   end
-end 
-
+end
 
 def selective_tweet_shortener(tweet)
   if tweet.chars.length > 140  
@@ -31,12 +38,15 @@ def selective_tweet_shortener(tweet)
   end 
 end 
 
-
-
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).chars.length > 140  
-    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
-  else 
-    word_substituter(tweet) 
-  end 
-end 
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
+
+
+
