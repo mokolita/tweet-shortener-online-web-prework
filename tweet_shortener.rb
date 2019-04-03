1fcd42433e4c3c7a4e45e14dda0627435b1a704b
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:20:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,9 +12,16 @@ dictionary = {:hello => "hi"
 
 end 
 
-def word_substituter(string)
-  array = string.to_a 
-  array.each do |
 
 
-end 
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
