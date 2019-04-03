2d39bd9132e039d5b008035d1c74ccd8670c90fc
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:11:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,7 +4,11 @@ dictionary = {:hello => "hi"
               :two => "2"
               :too => "2"
               :for => "4"
-              
+              :four => "4"
+              :be => "b"
+              :you => "u"
+              :at => "@"
+              :and => "&"}
 
 end 
 
