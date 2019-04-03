e83bcff65eda251aae1d073ad536a34093279303
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:10:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def dictionary
+dictionary = {:hello => "hi"
+              :to => "2"
+              :two => "2"
+              :too => "2"
+              :for => "4"
+              
+
+end 
+
+
+
+
+
+
+def word_substituter(string)
+
+
+
+end 
