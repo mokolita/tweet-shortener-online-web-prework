9227ec937f9b243a7f8b4f7711b4ebe3aef613cb
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:22:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,36 +1,4 @@
-def dictionary
-dictionary = {:hello => "hi",
-              :to => "2",
-              :two => "2",
-              :too => "2",
-              :for => "4",
-              :four => "4",
-              :be => "b",
-              :you => "u",
-              :at => "@",
-              :and => "&"}
 
-end 
-
-
-
-def word_substituter(tweet)
-  # replaces long words with their expected short form
-
-    tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else
-        word
-    end
-  end.join(" ")
-end
-
-
-phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-
-# Write your code here.
-require "pry"
 
 def dictionary
   convert = {
