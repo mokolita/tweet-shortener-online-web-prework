54d72453a7046eeaff111f3f9baeedf8af26b300
Meg Galvez <meg@oakmaine.com>
2019-04-03 16:20:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,13 @@
 def dictionary
-dictionary = {:hello => "hi"
-              :to => "2"
-              :two => "2"
-              :too => "2"
-              :for => "4"
-              :four => "4"
-              :be => "b"
-              :you => "u"
-              :at => "@"
+dictionary = {:hello => "hi",
+              :to => "2",
+              :two => "2",
+              :too => "2",
+              :for => "4",
+              :four => "4",
+              :be => "b",
+              :you => "u",
+              :at => "@",
               :and => "&"}
 
 end 
